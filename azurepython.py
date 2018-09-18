e84f9fdd773abf7dfdbb6cbb74a25f942d4f2cb6
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-09-18 16:05:19 +0530

    Update azurepython.py

--- azurepython.py
+++ azurepython.py
@@ -13,31 +13,23 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 import re
 from flask import make_response
 app = Flask(__name__)
-english_bot = ChatBot("GUI Bot", read_only=True,
-                      preprocessors=[
-                          'chatterbot.preprocessors.clean_whitespace'
-                      ],
-
-                      logic_adapters=[
-                                         {
-                                             'import_path': 'chatterbot.logic.BestMatch',
-                                             "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
-                                             "statement_comparison_function1": "chatterbot.comparisons.SentimentComparison",
-                                             "statement_comparison_function2": "chatterbot.comparisons.SynsetDistance",
-                                             "response_selection_method": "chatterbot.response_selection.get_random_response"
-                                         },
+english_bot = ChatBot("GUI Bot", read_only=True, preprocessors=['chatterbot.preprocessors.clean_whitespace'], logic_adapters=[
+	{
+  'import_path': 'chatterbot.logic.BestMatch',
+   "statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
+   "statement_comparison_function1": "chatterbot.comparisons.SentimentComparison",
+   "statement_comparison_function2": "chatterbot.comparisons.SynsetDistance",
+   "response_selection_method": "chatterbot.response_selection.get_random_response"
+        },
 {
         'import_path': 'chatterbot.logic.LowConfidenceAdapter',
         'threshold': 0.85,
         'default_response': 'I am sorry, but I do not understand.'
         }
-                            # "chatterbot.logic.BestMatch"
-
         ],
 		storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
 		input_adapter="chatterbot.input.VariableInputTypeAdapter",
                 output_adapter="chatterbot.output.OutputAdapter",
-		filters =    ['chatterbot.filters.RepetitiveResponseFilter'],
                 database="db.sqlite3"
       )
 @app.route('/')
