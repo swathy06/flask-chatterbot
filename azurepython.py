f6e667b9c0d8a3aa7268ccefe216d98550ac9a89
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-09-18 12:19:19 +0530

    Update azurepython.py

--- azurepython.py
+++ azurepython.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hey its Python Flask application Running!'
-    #return render_template("index.html")
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
